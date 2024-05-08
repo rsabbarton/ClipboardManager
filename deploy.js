753be46d650e6cd194fbfc38c58bbf310319394a
Richard Sabbarton <richard.sabbarton@gmail.com>
2024-05-08 11:35:28 +0100

    Update deploy script

--- deploy.js
+++ deploy.js
@@ -7,12 +7,20 @@ let dest = path.join(__dirname, 'docs', 'downloads')
 
 // deploy to website
 
-let installerNameDMGx64 = `${package.name}-macOS-x64-v${package.version}.dmg`
 
-// Mac OS Intel DMG
+// Mac OS x64 DMG
 if(fs.existsSync(path.join(src, 'make', 'zip', 'darwin', 'x64'))){
+    let installerName = `${package.name}-macOS-x64-v${package.version}.dmg`
     if(fs.existsSync(path.join(src, 'make', 'Clipboard Manager.dmg'))){
-        fs.copyFileSync(path.join(src, 'make', 'Clipboard Manager.dmg'), path.join(dest, installerNameDMGx64))
+        fs.copyFileSync(path.join(src, 'make', 'Clipboard Manager.dmg'), path.join(dest, installerName))
+    }
+}
+
+// Mac OS arm64 DMG
+if(fs.existsSync(path.join(src, 'make', 'zip', 'darwin', 'arm64'))){
+    let installerName = `${package.name}-macOS-arm64-v${package.version}.dmg`
+    if(fs.existsSync(path.join(src, 'make', 'Clipboard Manager.dmg'))){
+        fs.copyFileSync(path.join(src, 'make', 'Clipboard Manager.dmg'), path.join(dest, installerName))
     }
 }
 
