f6a72a890f42410c6d2d7936b5649e9f490a4480
Richard Sabbarton <richard.sabbarton@gmail.com>
2024-05-08 14:59:26 +0100

    build sync

--- deploy.js
+++ deploy.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const fs = require('fs')
-let package = JSON.parse(fs.readFileSync(path.join(__dirname, 'package.json')))
+let package = require('package.json')
+let build = require('build.json')
 
 let src = path.join(__dirname, 'out')
 let dest = path.join(__dirname, 'docs', 'downloads')
@@ -10,7 +11,7 @@ let dest = path.join(__dirname, 'docs', 'downloads')
 
 // Mac OS x64 DMG
 if(fs.existsSync(path.join(src, 'make', 'zip', 'darwin', 'x64'))){
-    let installerName = `${package.name}-macOS-x64-v${package.version}.dmg`
+    let installerName = `${package.name}-macOS-x64-v${package.version}_build-${build.buildNumber}.dmg`
     if(fs.existsSync(path.join(src, 'make', 'Clipboard Manager.dmg'))){
         fs.copyFileSync(path.join(src, 'make', 'Clipboard Manager.dmg'), path.join(dest, installerName))
     }
@@ -18,7 +19,7 @@ if(fs.existsSync(path.join(src, 'make', 'zip', 'darwin', 'x64'))){
 
 // Mac OS arm64 DMG
 if(fs.existsSync(path.join(src, 'make', 'zip', 'darwin', 'arm64'))){
-    let installerName = `${package.name}-macOS-arm64-v${package.version}.dmg`
+    let installerName = `${package.name}-macOS-arm64-v${package.version}_build-${build.buildNumber}.dmg`
     if(fs.existsSync(path.join(src, 'make', 'Clipboard Manager.dmg'))){
         fs.copyFileSync(path.join(src, 'make', 'Clipboard Manager.dmg'), path.join(dest, installerName))
     }
